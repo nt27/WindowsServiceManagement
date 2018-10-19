59bc59781205a13322852b85fd51f967e746a33e
Navaneethakrishnan <navaneethakrishnan@live.com>
2018-10-19 21:17:00 +0000

    Added the web application

---
+++ WindowsServiceManagement/ServiceManagement-WebApp/WebPage.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebPage.aspx.cs" Inherits="ServiceManagement_WebApp.WebPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Service Monitoring</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div class="container-fluid table-bordered">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header text-left text-success">
+                        <h3>Application Title
+				</h3>
+                        <br class="border-right-0" />
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header text-left text-success">
+                        <asp:Button ID="btnRefresh" runat="server" Text="Refresh" CssClass="btn-link btn-sm" OnClick="btnRefresh_Click" />
+                    </div>
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col-md-6">
+                    <h4 class="text-left text-danger">Non-Production Environments
+                    </h4>
+                    <div class="small">
+                        <asp:GridView ID="grdPreProdDetails" runat="server" CellPadding="3" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px">
+                            <FooterStyle BackColor="White" ForeColor="#000066" />
+                            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                            <RowStyle ForeColor="#000066" />
+                            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                            <SortedDescendingHeaderStyle BackColor="#00547E" />
+                        </asp:GridView>
+                    </div>
+
+                </div>
+                <div class="col-md-6">
+                    <h4 class="text-left text-danger">Production Environments
+                    </h4>
+                    <div class="small">
+                        <asp:GridView ID="grdPrdDetails" runat="server" CellPadding="3" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px">
+                            <FooterStyle BackColor="White" ForeColor="#000066" />
+                            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                            <PagerStyle ForeColor="#000066" HorizontalAlign="Left" BackColor="White" />
+                            <RowStyle ForeColor="#000066" />
+                            <SelectedRowStyle Font-Size="X-Small" BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                            <SortedDescendingHeaderStyle BackColor="#00547E" />
+                        </asp:GridView>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </form>
+</body>
+</html>
---
+++ WindowsServiceManagement/ServiceManagement-WebApp/WebPage.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Xml;
+using System.Data;
+using System.Management.Automation;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+
+namespace ServiceManagement_WebApp
+{
+    public partial class WebPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadServiceDetailsInGrid();
+        }
+        private string ExecutePowerShellScript(string strServerName, string strServiceName, string strAction)
+        {
+            try
+            {
+                string strScriptFilePath = ConfigurationManager.AppSettings["PowerShellFilePath"].ToString();
+                using (PowerShell PowerShellInstance = PowerShell.Create())
+                {
+                    System.IO.StreamReader sr = new System.IO.StreamReader(strScriptFilePath);
+                    string strFileContent = sr.ReadToEnd();
+                    PowerShellInstance.AddScript(strFileContent);
+                    PowerShellInstance.AddParameter("ServerName", strServerName);
+                    PowerShellInstance.AddParameter("WindowsServiceName", strServiceName);
+                    PowerShellInstance.AddParameter("Action", strAction);
+                    Collection<PSObject> PSOutput = PowerShellInstance.Invoke();
+                    string strOutput = "";
+                    foreach (PSObject outputItem in PSOutput)
+                    {
+                        if (outputItem != null)
+                        {
+                            strOutput += outputItem.BaseObject.ToString() + "\n";
+                        }
+                    }
+                    return strOutput;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+                return null;
+            }
+        }
+        private void LoadServiceDetailsInGrid()
+        {
+            try
+            {
+                //Data Table Columns
+                DataTable dtServiceDetails = new DataTable();
+                dtServiceDetails.Columns.Add("Environment", typeof(string));
+                dtServiceDetails.Columns.Add("ServerName", typeof(string));
+                dtServiceDetails.Columns.Add("ServiceName", typeof(string));
+                dtServiceDetails.Columns.Add("CurrentStatus", typeof(string));
+
+                string strServiceConfigFilePath = ConfigurationManager.AppSettings["ServiceConfigFilePath"].ToString();
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(strServiceConfigFilePath);
+
+                foreach (XmlNode xmlNodeEnv in xmlDoc.DocumentElement.SelectNodes("/WindowsServiceDetails/Environment"))
+                {
+                    string strEnvironment = xmlNodeEnv.Attributes["Name"].InnerText;
+                    XmlDocument xmlDocService = new XmlDocument();
+                    for (int i = 0; i <= xmlNodeEnv.ChildNodes.Count - 1; i++)
+                    {
+                        string strServerName = xmlNodeEnv.ChildNodes[i].Attributes["ServerName"].InnerText;
+                        string strServiceName = xmlNodeEnv.ChildNodes[i].Attributes["ServiceName"].InnerText;
+                        dtServiceDetails.Rows.Add(strEnvironment, strServerName, strServiceName, "Status");
+                    }
+                }
+                foreach (DataRow dtRow in dtServiceDetails.Rows)
+                {
+                    string strServerName = dtRow[1].ToString();
+                    string strServiceName = dtRow[2].ToString();
+                    string strAction = "Status";
+
+                    dtRow[3] = ExecutePowerShellScript(strServerName, strServiceName, strAction);
+                }
+
+                //grdServiceDetails.DataSource = null;
+                //grdServiceDetails.Columns.Clear();
+                //grdServiceDetails.Rows.cle
+
+
+                grdPreProdDetails.DataSource = dtServiceDetails;
+                grdPreProdDetails.DataBind();
+
+                grdPrdDetails.DataSource = dtServiceDetails;
+                grdPrdDetails.DataBind();
+
+                /* for (int i = 0; i <= grdServiceDetails.Rows.Count - 1; i++)
+                 {
+                     string strCurrentStatus = grdServiceDetails.Rows[i].Cells[3].Value.ToString();
+                     strCurrentStatus = Regex.Replace(strCurrentStatus, @"\t|\n|\r", "");
+                     if (strCurrentStatus == "Running")
+                     {
+                         grdServiceDetails.Rows[i].DefaultCellStyle.ForeColor = Color.Green;
+                     }
+                     else if (strCurrentStatus == "Stopped")
+                     {
+                         grdServiceDetails.Rows[i].DefaultCellStyle.ForeColor = Color.Red;
+                     }
+                 }*/
+
+                /*if (grdServiceDetails.Columns.Count == 4)
+               {
+                  DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+                   grdServiceDetails.Columns.Add(btn);
+                   btn.HeaderText = "Action";
+                   btn.Text = "Start / Stop";
+                   btn.Name = "btnAction";
+                   btn.UseColumnTextForButtonValue = true;
+            }*/
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+        }
+
+        protected void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadServiceDetailsInGrid();
+        }
+    }
+}
